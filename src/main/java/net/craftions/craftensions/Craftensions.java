1ab7cba2f25f1de01eec79551ad1bdc10b9a63a9
MCTzOCK <dev@mctzock.de>
2021-03-11 18:24:54 +0100

    Set executor for command extension

--- src/main/java/net/craftions/craftensions/Craftensions.java
+++ src/main/java/net/craftions/craftensions/Craftensions.java
@@ -1,17 +1,17 @@
 package net.craftions.craftensions;
 
+import net.craftions.craftensions.commands.CommandExtension;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Craftensions extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getCommand("extension").setExecutor(new CommandExtension());
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
