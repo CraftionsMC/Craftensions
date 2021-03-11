c717dd592214f981c0acd7d23b0d3fa036d57b5b
MCTzOCK <dev@mctzock.de>
2021-03-11 18:14:00 +0100

    First commit

---
+++ src/main/java/net/craftions/craftensions/Craftensions.java
@@ -0,0 +1,17 @@
+package net.craftions.craftensions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Craftensions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
