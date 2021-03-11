015ca3e6707fffc27d9437618665064ea7755b14
MCTzOCK <dev@mctzock.de>
2021-03-11 18:26:18 +0100

    Set tab completer for extension command

--- src/main/java/net/craftions/craftensions/Craftensions.java
+++ src/main/java/net/craftions/craftensions/Craftensions.java
@@ -1,6 +1,7 @@
 package net.craftions.craftensions;
 
 import net.craftions.craftensions.commands.CommandExtension;
+import net.craftions.craftensions.tabcompleter.CommandExtensionTabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Craftensions extends JavaPlugin {
@@ -8,6 +9,7 @@ public final class Craftensions extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("extension").setExecutor(new CommandExtension());
+        getCommand("extension").setTabCompleter(new CommandExtensionTabCompleter());
     }
 
     @Override
