33e35d42ee2343f620e48566400e6141bed53446
MCTzOCK <dev@mctzock.de>
2021-03-12 16:42:55 +0100

    Added gamemode command

---
+++ FancyCommands/src/main/java/net/craftions/fancycommands/commands/CommandGameMode.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021 Ben Siebert. All rights reserved.
+ */
+package net.craftions.fancycommands.commands;
+
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandGameMode implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player){
+            if(args[0].equals("0") || args[0].startsWith("su")) {
+                ((Player) sender).setGameMode(GameMode.SURVIVAL);
+                sender.sendMessage("§bYour gamemode has been updated to §esurvival");
+            }else if(args[0].equals("1") || args[0].startsWith("c")) {
+                ((Player) sender).setGameMode(GameMode.CREATIVE);
+                sender.sendMessage("§bYour gamemode has been updated to §ecreative");
+            }else if(args[0].equals("2") || args[0].startsWith("a")) {
+                ((Player) sender).setGameMode(GameMode.ADVENTURE);
+                sender.sendMessage("§bYour gamemode has been updated to §eadventure");
+
+            }else if (args[0].equals("3") || args[0].startsWith("sp")) {
+                ((Player) sender).setGameMode(GameMode.SPECTATOR);
+                sender.sendMessage("§bYour gamemode has been updated to §espectator");
+            }else {
+                sender.sendMessage("§cPlease use §b/gamemode <0|1|2|3|survival|creative|adventure|spectator>");
+            }
+        }else {
+            sender.sendMessage("§cYou need to be a player.");
+        }
+        return true;
+    }
+}
