225423851f26e5499feb50c6b194a2a4ca421c1f
MCTzOCK <dev@mctzock.de>
2021-03-13 12:03:14 +0100

    Added CButils Module

---
+++ CBUtils/src/main/java/net/craftions/cbutils/CBUtils.java
@@ -0,0 +1,17 @@
+package net.craftions.cbutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CBUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
