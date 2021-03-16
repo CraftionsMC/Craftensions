f33a046b94d6c08bcf7fea00fed484c23852d669
MCTzOCK <dev@mctzock.de>
2021-03-16 15:40:49 +0100

    Added MySQL

--- ActivationKeys/src/main/java/net/craftions/activationkeys/util/MySQL.java
+++ ActivationKeys/src/main/java/net/craftions/activationkeys/util/MySQL.java
@@ -3,7 +3,7 @@
  */
 package net.craftions.activationkeys.util;
 
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class MySQL {
 
@@ -11,10 +11,35 @@ public class MySQL {
 
     public MySQL(String sql){
         this.sql = sql;
-        this.execute();
     }
 
-    public void execute (){
-        
+    public ResultSet executeQuery(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(this.getConnectionString(), Config.mysql_username, Config.mysql_password);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            con.close();
+            return rs;
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public void executeUpdate() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(this.getConnectionString(), Config.mysql_username, Config.mysql_password);
+            Statement st = con.createStatement();
+            st.executeUpdate(sql);
+            con.close();
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private String getConnectionString(){
+        return "jdbc:mysql://" + Config.mysql_host + ":" + Config.mysql_port + "/" + Config.mysql_database;
     }
 }
